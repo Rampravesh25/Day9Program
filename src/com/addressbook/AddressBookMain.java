551e6b5ee6d2160beaf36f151910c197a70079b6
Rampravesh25 <rampraveshmallah@gmail.com>
2022-06-11 19:37:04 +0530

    UC3 Program

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -9,6 +9,24 @@ public class AddressBookMain {
         a1.contactList();
         a1.contactList();
         System.out.println(a1.listData);
-    }
 
+        System.out.println("if you want to change person Name , reply yes/no : ");
+        String check = AddressBook.scan.nextLine();
+
+        System.out.println("enter name whose details you want to edit : ");
+        String name = AddressBook.scan.nextLine();
+
+        if (check.equals("yes")) {
+            for (Contacts cThrough : a1.listData) {
+
+                if (cThrough.getFirstName().equals(name)) {
+                    System.out.println("enter updated phone number : ");
+                    cThrough.setPhone(AddressBook.scan.next());
+                    break;
+                }
+            }
+        }
+        System.out.println(a1.listData);
+    }
 }
+
